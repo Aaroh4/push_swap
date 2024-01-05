1d0a9bf1f73fb1880039492b471e28f3b4e56913
Aaro Hamalainen <ahamalai@c1r1p10.hive.fi>
2024-01-05 17:00:41 +0200

    Added new code

---
+++ error_handling.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handling.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahamalai <ahamalai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 15:55:43 by ahamalai          #+#    #+#             */
+/*   Updated: 2024/01/05 16:42:56 by ahamalai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	free_matrix(char **argv)
+{
+	int	i;
+	i = -1;
+	if (NULL == argv || NULL == *argv)
+		return ;
+	while (argv[i])
+		free(argv[i++];
+	free(argv - 1);
+}
+
+void	error_free(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	free_stack(a);
+	if (fag_argc_2)
+		free_matrix(argv);
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
+int	error_syntax(char *str_nbr)
+{
+	if (!(*str_nbr == '+'
+				|| *str_nbr == '-'
+				|| (*str_nbr >= '0' && *str_nbr <= '9')))
+		return (1);
+	if ((*str_nbr == '+'
+				|| *str_nbr == '-')
+			&& !(str_nbr[1] >= '0' && str_nbr[1] <= '9'))
+		return (1);
+	while (*++str_nbr)
+	{
+		if (!(*str_nbr >= '0' && *str_nbr <= '9'))
+			return (1);
+	}
+	return (0);
+}
+
+int	error_repetition(t_stack_node *a, int nbr)
+{
+	if (NULL == a)
+		return (0);
+	while (a)
+	{
+		if (a->value == nbr)
+			return (1);
+		a = a->next;
+	}
+	return (0);
+}
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ahamalai <ahamalai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:31:20 by ahamalai          #+#    #+#             */
-/*   Updated: 2024/01/04 14:18:07 by ahamalai         ###   ########.fr       */
+/*   Updated: 2024/01/05 17:00:21 by ahamalai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -22,5 +22,51 @@ int	main(int argc, char **argv)
 		return (1);
 	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-	init_stack_a(&a, argv + 1);
+	init_stack_(&a, argv + 1,  2 == argc);
+}
+
+static long	ft_atol(const char *str)
+{
+	long	num;
+	int		isneg;
+	int		i;
+
+	num = 0;
+	isneg = 1;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == '\f'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * isneg);
+}
+
+void	init_stack(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
+			error_free(a, argv, flag_argc_2);
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error_free(a,  argv, flag_arc_2);
+		if (error_repetition(*a, (int)nbr))
+			error_free(a, argv, flag_arc_2);
+	}
 }
